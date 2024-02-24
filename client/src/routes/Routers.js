0095ee50f86b9fd517921571f61a3a9d6b18f1b0
KrishnaKhadke <krishnakhadke11@gmail.com>
2024-02-25 03:06:44 +0530

    Created Quiz,quizData,general animal Data Add

--- client/src/routes/Routers.js
+++ client/src/routes/Routers.js
@@ -3,7 +3,7 @@ import Home from '../pages/Home';
 import News from '../components/news/News'
 import Events from '../components/events/Events'
 import Santuries from '../components/santuries/Santuries';
-
+import Quiz from '../components/quiz/Quiz.jsx'
 const Routers = () => {
   return (
    <Routes>
@@ -11,6 +11,7 @@ const Routers = () => {
     <Route path='/news' element={<News/>} />
     <Route path='/events' element={<Events/>} />
     <Route path='/santuries' element={<Santuries/>} />
+    <Route path='/quiz' element={<Quiz/>} />
    </Routes>
   )
 }
