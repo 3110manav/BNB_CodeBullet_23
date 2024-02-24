0cbf73460d55e0801f2f1f9c931b0b5a883ac8d3
vedjaiswal <vedjaiswal0506@gmail.com>
2024-02-25 03:36:09 +0530

    event initiated

--- client/src/components/events/EventCard.js
+++ client/src/components/events/EventCard.js
@@ -10,29 +10,29 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 function EventCard() {
 
     return (
-      <div className="flex items-center justify-center">
-  <div className="bg-white shadow-lg rounded-md overflow-hidden w-1050">
-    <img
-      className="h-250 w-1/3 object-cover rounded-l-md"
-      src="https://images.unsplash.com/photo-1473260079709-83c808703435?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      alt="Live from space album cover"
-    />
-    <div className="flex flex-col p-4 w-2/3">
-      <div className="font-semibold text-xl mb-2">Safari for Conservation</div>
-      <div className="text-green-500 mb-2">
-        <CalendarMonthIcon className="inline-block w-5 h-5 mr-2" />
-        Saturday, March 15th, 2024, 10:00 AM - 2:00 PM
+      <div style={{width:1000}}>
+        <div className="flex bg-white shadow-lg rounded-md overflow-hidden">
+            <img
+            className="h-250 w-1/3 object-cover rounded-l-md"
+            src="https://images.unsplash.com/photo-1473260079709-83c808703435?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Live from space album cover"
+            />
+            <div className="flex flex-col p-4 w-2/3">
+            <div className="font-semibold text-xl mb-2">Safari for Conservation</div>
+            <div className="text-green-500 mb-2">
+                <CalendarMonthIcon className="inline-block w-5 h-5 mr-2" />
+                Saturday, March 15th, 2024, 10:00 AM - 2:00 PM
+            </div>
+            <div className="text-green-500 mb-2">
+                <LocationOnIcon className="inline-block w-5 h-5 mr-2" />
+                Serengeti National Park, Tanzania
+            </div>
+            <div>
+                Embark on an unforgettable journey through the heart of the Serengeti as we come together to support wildlife conservation efforts. Our Safari for Conservation event will immerse you in the beauty of nature while raising vital funds to protect endangered species and their habitats.
+            </div>
+            </div>
+        </div>
       </div>
-      <div className="text-green-500 mb-2">
-        <LocationOnIcon className="inline-block w-5 h-5 mr-2" />
-        Serengeti National Park, Tanzania
-      </div>
-      <div>
-        Embark on an unforgettable journey through the heart of the Serengeti as we come together to support wildlife conservation efforts. Our Safari for Conservation event will immerse you in the beauty of nature while raising vital funds to protect endangered species and their habitats.
-      </div>
-    </div>
-  </div>
-</div>
 
     );
 }
--- client/src/routes/Routers.js
+++ client/src/routes/Routers.js
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import News from '../components/news/News'
 import Events from '../components/events/Events'
-import Santuries from '../components/santuries/Santuries';
 
 const Routers = () => {
   return (
@@ -10,7 +9,6 @@ const Routers = () => {
     <Route path='/' element={<Home />} />
     <Route path='/news' element={<News/>} />
     <Route path='/events' element={<Events/>} />
-    <Route path='/santuries' element={<Santuries/>} />
    </Routes>
   )
 }
