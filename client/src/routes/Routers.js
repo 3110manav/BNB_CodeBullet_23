b0646e923c560f50519ca66cc1ee7024b502ada5
vedjaiswal <vedjaiswal0506@gmail.com>
2024-02-24 21:16:51 +0530

    event route added

--- client/src/routes/Routers.js
+++ client/src/routes/Routers.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import News from '../components/news/News'
+import Events from '../components/events/Events'
 
 const Routers = () => {
   return (
    <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/news' element={<News/>} />
+    <Route path='/events' element={<Events/>} />
    </Routes>
   )
 }
