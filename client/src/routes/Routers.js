dfdbe1c6777dddb17b9c78bf8a7fc657d2cf5563
vedjaiswal <vedjaiswal0506@gmail.com>
2024-02-24 21:15:02 +0530

    mui added

--- client/src/routes/Routers.js
+++ client/src/routes/Routers.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import News from '../components/news/News'
 
 const Routers = () => {
   return (
    <Routes>
     <Route path='/' element={<Home />} />
+    <Route path='/news' element={<News/>} />
    </Routes>
   )
 }
