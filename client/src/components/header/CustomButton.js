3a801d5a93118fb067d54c007a50d57bba6a36ad
3110manav <81803132+3110manav@users.noreply.github.com>
2024-02-25 13:23:45 +0530

    Login Fix

--- client/src/components/header/CustomButton.js
+++ client/src/components/header/CustomButton.js
@@ -15,13 +15,13 @@ function CustomButton() {
     }
 
   return (
-    <div className="flex items-center lg:order-2">
+    <div className="flex items-center lg:order-2 ml-10">
         {
             account === '' &&
             <div>
                 <button
                     onClick={(e) => openDialog(e)}
-                    className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                    className="text-white bg-green-500 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                 >
                     Log in
                 </button>
--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react'
-import {styled, Box} from '@mui/material'
+import React, { useState } from "react";
+import { styled, Box } from "@mui/material";
+import dogGif from "../../assets/images/DogPaw.gif";
 
-
-import Login from './Login'
-import Signup from './Signup'
-import { Dialog } from '@mui/material'
-
+import Login from "./Login";
+import Signup from "./Signup";
+import { Dialog } from "@mui/material";
 
 // const Container = styled(Box)`
 //  height : 100vh;
@@ -16,49 +15,47 @@ import { Dialog } from '@mui/material'
 //  background-size : cover;
 // `
 const FormContainer = styled(Box)`
-  height : 600px;
-  width : 900px;
+  height: 600px;
+  width: 900px;
   display: grid;
-  position : relative;
-  background-color : #FBFAFF;
-//   border-radius : 20px;
--webkit-box-shadow: 8px 8px 20px -3px rgba(4,4,4,1);
--moz-box-shadow: 8px 8px 20px -3px rgba(4,4,4,1);
-box-shadow: 8px 8px 20px -3px rgba(4,4,4,1);
-  grid-template-areas : 
-    'left right'
-    'left right';
-  
-`
+  position: relative;
+  background-color: #fbfaff;
+  //   border-radius : 20px;
+  -webkit-box-shadow: 8px 8px 20px -3px rgba(4, 4, 4, 1);
+  -moz-box-shadow: 8px 8px 20px -3px rgba(4, 4, 4, 1);
+  box-shadow: 8px 8px 20px -3px rgba(4, 4, 4, 1);
+  grid-template-areas:
+    "left right"
+    "left right";
+`;
 
 const LeftContainer = styled(Box)`
-  grid-area : left;
-  overflow : hidden;
-//   border-radius : 0px 0px 0px 20px;
-//   margin-left : 30px;
-`
+  grid-area: left;
+  overflow: hidden;
+  //   border-radius : 0px 0px 0px 20px;
+  //   margin-left : 30px;
+`;
 const Header = styled(Box)`
-  grid-area : logo;
-  height : 90px;
-`
-const HeaderImage = styled('img')({
-  marginLeft:'35%',
+  grid-area: logo;
+  height: 90px;
+`;
+const HeaderImage = styled("img")({
+  marginLeft: "35%",
   height: 120,
-  position:'absolute'
-})
+  position: "absolute",
+});
 
-const LoginImage = styled('img')`
+const LoginImage = styled("img")`
   display: block;
-  margin-left: auto;
+  // margin-left: auto;
   margin-right: auto;
-//   height : 400px;
-//   width : 300px;
+  //   height : 400px;
+  //   width : 300px;
   object-fit: cover;
-`
+`;
 
 function Register({ open, setOpen, login }) {
- 
-    const [loginaccount, toggleAccount] = useState(login);
+  const [loginaccount, toggleAccount] = useState(login);
 
   const handleClose = () => {
     setOpen(false);
@@ -69,27 +66,30 @@ function Register({ open, setOpen, login }) {
     toggleAccount(!loginaccount);
   };
 
-
   return (
     <div>
-        <Dialog
+      <Dialog
         open={open}
         onClose={handleClose}
         PaperProps={{ sx: { maxWidth: "unset" } }}
-        >
-            {/* <Container> */}
-                <FormContainer>
-                <LeftContainer>
-                    <LoginImage src="https://images.unsplash.com/photo-1520552159191-e28a1d9f0d7e?q=80&w=1909&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="illustration" />
-                    </LeftContainer>
-                    <div style={{ height:600, display:"flex"}}>
-                        {loginaccount ? <Login toggle={toggle} handleClose={handleClose}/> : <Signup toggle={toggle} handleClose={handleClose}/>}
-                    </div>
-                </FormContainer>
-            {/* </Container> */}
-            </Dialog>
+      >
+        {/* <Container> */}
+        <FormContainer >
+          <LeftContainer className="bg-white">
+            <LoginImage src={dogGif} alt="illustration" className="w-full m-5 mt-10 p-5" />
+          </LeftContainer>
+          <div style={{ height: 600, display: "flex" , background:"white"}}>
+            {loginaccount ? (
+              <Login toggle={toggle} handleClose={handleClose} />
+            ) : (
+              <Signup toggle={toggle} handleClose={handleClose} />
+            )}
+          </div>
+        </FormContainer>
+        {/* </Container> */}
+      </Dialog>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
